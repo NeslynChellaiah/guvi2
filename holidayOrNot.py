e17a8b4488c0de900d6747571c3896154a00ff8a
NeslynChellaiah <36042097+NeslynChellaiah@users.noreply.github.com>
2019-03-19 12:11:02 +0530

    Create holidayOrNot.py

---
+++ holidayOrNot.py
@@ -0,0 +1,10 @@
+class main:
+    def func(self):
+        a = (input())
+        if (a=="Sunday") or (a=="Saturday"):
+            print ("yes")
+        else:
+            print("no")
+            print()
+ob =  main()
+ob.func()
