bf13f0a228bd22f51eae5577dc4e4d3c7680db7e
Neslyn Chellaiah <neslynchellaiah@gmail.com>
2019-03-20 12:12:05 +0530

    Update 3Points.py

--- 3Points.py
+++ 3Points.py
@@ -5,6 +5,8 @@ class main():
         e,f=input().split()
         if (a==c==e) or (b==d==f):
             print ("yes")
+        elif(a==b)or(c==d)or(e==f):
+            print ("yes")
         else:
             print ("no")
 ob = main()
